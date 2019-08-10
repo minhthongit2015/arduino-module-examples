3389fa7e314b55d85e5a19c3d8dc76f4bc7eeeac
Thong Tran <minhthong.it2015@gmail.com>
2019-08-10 15:20:17 +0700

    update

--- Modules/TTP229/build/core/libb64/cdecode.c.d
+++
@@ -1,7 +0,0 @@
-k:\Arrows\Raspberry\ Pi\ Tutorial\Modules\TTP229\build\core\libb64\cdecode.c.o: \
- C:\Users\KID\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266\libb64\cdecode.c \
- C:\Users\KID\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266\libb64\cdecode.h \
- C:\Users\KID\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/stdint.h \
- C:\Users\KID\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/machine/_default_types.h \
- C:\Users\KID\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/features.h \
- C:\Users\KID\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/_intsup.h
